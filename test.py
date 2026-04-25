cf160f0f2c2d9a7ba005856c4e6f130d86733e09
John Aldrie Baquiran <aldriebaquiran15@gmail.com>
2026-04-25 16:04:57 +0800

    fix

--- test.py
+++ test.py
@@ -2,20 +2,23 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import time
- 
+
 options = Options()
+options.binary_location = "/usr/bin/google-chrome" # The path from your install output
 options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
- 
+
 service = Service("/usr/bin/chromedriver")
 driver = webdriver.Chrome(service=service, options=options)
- 
-driver.get("http://localhost")
-time.sleep(2)
- 
-assert "Hello CI/CD World" in driver.page_source
-print("TEST PASSED")
- 
-driver.quit()
+
+try:
+    driver.get("http://localhost")
+    time.sleep(2)
+    assert "Hello CI/CD World" in driver.page_source
+    print("TEST PASSED")
+except Exception as e:
+    print(f"TEST FAILED: {e}")
+finally:
+    driver.quit()
